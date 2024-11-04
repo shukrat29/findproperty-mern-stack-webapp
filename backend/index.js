94b6c162c09a62da8095c4577a5b12a189b00374
Shukra Tamang <shukratamang.npl@gmail.com>
2024-11-04 12:46:53 +1100

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongodb connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
