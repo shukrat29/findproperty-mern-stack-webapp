502c3e3e81053f9a40d79100fe074954923d63f1
Shukra Tamang <shukratamang.npl@gmail.com>
2024-11-05 07:14:40 +1100

    create signup api

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,20 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import connectDB from "./config/db.js";
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("Mongodb connected successfully");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+connectDB();
 
 const app = express();
+
+// middlewares
+app.use(express.json());
+
+// Routes
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("Server running  on port 3000");
